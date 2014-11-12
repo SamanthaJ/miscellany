e82a629d9fa1cd3357fbe72c43c3b00063f512f5
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-11-12 14:50:55 -0500

    edits views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,6 @@ class CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id ])
     comment = post.comments.create(comment_params)
-    
-
     redirect_to post_path(comment.post)
   end
 
